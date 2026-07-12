dbf2611dda5c5f68def6188e6c2aeaf7c559a1eb
presley06 <presley06p@gmail.com>
2026-07-13 00:05:05 +0530

    Fix escHtml function

--- static/js/detect.js
+++ static/js/detect.js
@@ -685,3 +685,11 @@ function capitalize(str) {
         )
         .join(" ");
 }
+function escHtml(str) {
+    return String(str || "")
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#39;");
+}
